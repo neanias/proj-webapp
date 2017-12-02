4c2179effe862f5059593a63c852df867f754361
William Mathewson <wncmathewson@me.com>
2017-12-02 02:10:20 +0000

    Add basic drawing to index.ts

--- app/assets/js/index.ts
+++ app/assets/js/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello, world!");
+import * as d3 from "d3";
+import { chargePaths, IBlazon, ICharge } from "./charge_shapes";
+
+const svg = d3.select("svg");
+const shield = svg.select("#shield");
+const blazonConfig: IBlazon = {
+  charges: [
+    {
+      charge: "saltire",
+      tincture: "argent",
+    },
+  ],
+  field: "azure",
+};
+
+function drawShield(blazon: IBlazon): void {
+  shield.attr("class", blazon.field);
+  for (const charge of blazon.charges) {
+    svg.append("path")
+          .attr("id", charge.charge)
+          .attr("class", charge.tincture)
+          .attr("d", chargePaths[charge.charge]);
+  }
+}
+
+drawShield(blazonConfig);
