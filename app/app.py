9f4ec781dd92898d238960f6cefe497b98b6c72d
William Mathewson <wncmathewson@me.com>
2017-12-04 00:10:53 +0000

    Add dummy parsing to App

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,3 +7,47 @@ app = Flask(__name__)
 def index():
     """Base index page"""
     return render_template("index.html")
+
+
+@app.route("/_parse", methods=['POST'])
+def parse():
+    """Parse blazon text using 'backend'"""
+    blazon = request.get_json()['blazon']
+    return jsonify(parse_blazon(blazon))
+
+
+def parse_blazon(blazon):
+    """Dummy method in place of parser"""
+    payload = {}
+    if blazon.lower() == "scrope":
+        payload = {
+            "field": "azure",
+            "charges": [
+                {
+                    "charge": "bend",
+                    "tincture": "or"
+                }
+            ]
+        }
+    elif blazon.lower() == "st andrew":
+        payload = {
+            "field": "azure",
+            "charges": [
+                {
+                    "charge": "saltire",
+                    "tincture": "argent"
+                }
+            ]
+        }
+    else:
+        payload = {
+            "field": "gules",
+            "charges": [
+                {
+                    "charge": "bend_sinister",
+                    "tincture": "sable"
+                }
+            ]
+        }
+
+    return payload
