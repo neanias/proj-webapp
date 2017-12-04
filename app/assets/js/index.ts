d4b7e5b15d187506515ca747ebe6b2febb709763
William Mathewson <wncmathewson@me.com>
2017-12-04 00:28:38 +0000

    Add dynamic drawing of shields

--- app/assets/js/index.ts
+++ app/assets/js/index.ts
@@ -1,19 +1,12 @@
 import * as d3 from "d3";
+import * as $ from "jquery";
 import { chargePaths, IBlazon, ICharge } from "./charge_shapes";
 
 const svg = d3.select("svg");
 const shield = svg.select("#shield");
-const blazonConfig: IBlazon = {
-  charges: [
-    {
-      charge: "saltire",
-      tincture: "argent",
-    },
-  ],
-  field: "azure",
-};
 
 function drawShield(blazon: IBlazon): void {
+  clearShield();
   shield.attr("class", blazon.field);
   for (const charge of blazon.charges) {
     svg.append("path")
@@ -23,4 +16,31 @@ function drawShield(blazon: IBlazon): void {
   }
 }
 
-drawShield(blazonConfig);
+function clearShield(): void {
+  $("path").each((index, element) => {
+    if (!$(element).is("#shield")) {
+      element.remove();
+    }
+  });
+}
+
+// Capture form output, then send it to backend
+$(document).ready(() => {
+  $("form").submit((event) => {
+    const formData = {
+      blazon: $("textarea[name=blazon]").val(),
+    };
+    $.ajax({
+      contentType: "application/json; charset=UTF-8",
+      data: JSON.stringify(formData),
+      dataType: "json",
+      type: "POST",
+      url: "/_parse",
+    }).done((data) => {
+      drawShield(data);
+    }).fail((jqXHR, textStatus) => {
+      alert("Request failed: " + textStatus);
+    });
+    event.preventDefault();
+  });
+});
